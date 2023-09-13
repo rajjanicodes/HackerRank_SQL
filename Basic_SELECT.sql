7c8fb85d95b70e43021dd793c479b0e9d8c9ebdf
Raj Jani <92739995+rajjanicodes@users.noreply.github.com>
2023-09-13 16:06:55 +0530

    Added more spacing so that you don't accidentally check the next solution while referring one.
    Added more spacing so that you don't accidentally check the next solution while referring one.

--- Basic_SELECT.sql
+++ Basic_SELECT.sql
@@ -12,41 +12,141 @@ refer to            https://www.w3schools.com/sql/func_mysql_case.asp
 Doing HackerRank and LeetCode is the best way to learn SQL, so keep practicing.
 But refer to thee solutions only when you get stuck.
 
-
+EDIT: Added more spacing so that you don't accidentally check the next solution while referring one.
 */
+
+
 
 --Revising the Select Query I
 select * from CITY
 where POPULATION > 100000 AND COUNTRYCODE = 'USA';
 
+
+
+
+
+
+
+
+
+
+
 -- Revising the Select Query II
 SELECT NAME FROM CITY
 WHERE POPULATION > 120000 AND COUNTRYCODE = 'USA';
 
+
+
+
+
+
+
+
+
+
+
+
 -- Select All
 SELECT * FROM CITY;
+
+
+
+
+
+
+
+
+
+
+
 
 -- SELECT by ID
 SELECT * FROM CITY
 WHERE ID = 1661;
 
+
+
+
+
+
+
+
+
+
+
+
 -- Japanese Cities' Attributes
 SELECT * FROM CITY
 WHERE COUNTRYCODE = 'JPN';
+
+
+
+
+
+
+
+
+
+
+
 
 -- Japanese Cities' Names
 SELECT NAME FROM CITY
 WHERE COUNTRYCODE = 'JPN';
 
+
+
+
+
+
+
+
+
+
+
+
 -- Weather Observation Station 1
 SELECT CITY, STATE FROM STATION;
+
+
+
+
+
+
+
+
+
+
+
 
 -- Weather Observation Station 3
 SELECT DISTINCT CITY FROM STATION
 WHERE ID%2 = 0;
 
+
+
+
+
+
+
+
+
+
+
+
 -- Weather Observation Station 4
 SELECT COUNT(CITY) - COUNT(DISTINCT CITY) FROM STATION;
+
+
+
+
+
+
+
+
+
+
+
 
 -- Weather Observation Station 5
 SELECT CITY, LENGTH(CITY) FROM STATION
@@ -57,46 +157,167 @@ SELECT CITY, LENGTH(CITY) FROM STATION
 ORDER BY LENGTH(CITY) DESC, CITY
 LIMIT 1;
 
+
+
+
+
+
+
+
+
+
+
+
 -- Weather Observation Station 6
 SELECT DISTINCT CITY FROM STATION
 WHERE CITY REGEXP '^[AEIOU]';
+
+
+
+
+
+
+
+
+
+
+
 
 -- Weather Observation Station 7
 SELECT DISTINCT CITY FROM STATION
 WHERE CITY REGEXP '[AEIOU]$';
 
+
+
+
+
+
+
+
+
+
+
+
 -- Weather Observation Station 8
 SELECT DISTINCT CITY FROM STATION
 WHERE CITY REGEXP '^[AEIOU].*[AEIOU]$';
+
+
+
+
+
+
+
+
+
+
+
 
 -- Weather Observation Station 9
 SELECT DISTINCT CITY FROM STATION
 WHERE CITY REGEXP '^[^AEIOU]'
 
+
+
+
+
+
+
+
+
+
+
+
 -- Weather Observation Station 10
 SELECT DISTINCT CITY FROM STATION
 WHERE CITY REGEXP '[^AEIOU]$'
+
+
+
+
+
+
+
+
+
+
+
 
 -- Weather Observation Station 11
 SELECT DISTINCT CITY FROM STATION
 WHERE (CITY REGEXP '^[^AEIOU]') OR (CITY REGEXP '[^AEIOU]$');
 
+
+
+
+
+
+
+
+
+
+
+
 -- Weather Observation Station 12
 SELECT DISTINCT CITY FROM STATION
 WHERE CITY REGEXP '^[^AEIOU].*[^AEIOU]$';
+
+
+
+
+
+
+
+
+
+
+
 
 -- Higher Than 75 Marks
 SELECT Name FROM STUDENTS
 WHERE MARKS > 75
 ORDER BY RIGHT(NAME, 3), ID;
 
+
+
+
+
+
+
+
+
+
+
+
 -- Employee Names
 SELECT name FROM Employee
 ORDER BY name ASC;
 
+
+
+
+
+
+
+
+
+
+
+
 -- Employee Salaries
 SELECT NAME FROM EMPLOYEE
 WHERE (SALARY > 2000) AND (MONTHS < 10);
+
+
+
+
+
+
+
+
+
+
+
 
 -- Type of Triangle
 SELECT
